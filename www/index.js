070b6dfe6b3572f2727d78addbf2e25163d21d65
Anshul Kharbanda <403743-andydevs@users.noreply.replit.com>
2025-02-01 02:10:06 +0000

    draw SVG on canvas

--- www/index.js
+++ www/index.js
@@ -1,3 +1,35 @@
 import * as wasm from "rust-wasm";
+var svgpath = require('svgpath')
+const getBounds = require('svg-path-bounds')
 
-wasm.greet();
+// Components
+let svgPathTextArea = document.getElementById('svgpath')
+let canvas = document.getElementById('render-canvas')
+let renderBtn = document.getElementById('render-button')
+let ctx = canvas.getContext('2d')
+ctx.strokeStyle = '#ffffff'
+let width = canvas.width
+let height = canvas.height
+let originX = width/2
+let originY = height/2
+
+function getNewPath(e) {
+    // Read SVG path from textarea
+    let path = svgPathTextArea.value
+
+    // Transform SVG path
+    let [l, u, r, d] = getBounds(path)
+    let pathCenterX = (r - l)/2
+    let pathCenterY = (d - u)/2
+    let transformed = svgpath(path)
+        .translate(originX - pathCenterX, originY - pathCenterY)
+        .toString()
+
+    return transformed
+}
+
+
+
+// Display
+let path = getNewPath({})
+ctx.stroke(new Path2D(path))
