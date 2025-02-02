87965d624496872ebc20342b9d20827b302bb906
Anshul Kharbanda <403743-andydevs@users.noreply.replit.com>
2025-02-02 01:42:32 +0000

    Animate fourier motion path and arms

--- rust-wasm/src/lib.rs
+++ rust-wasm/src/lib.rs
@@ -4,7 +4,9 @@ use wasm_bindgen::prelude::*;
 use std::f64::consts::PI;
 use js_sys::Math::random;
 
-const PHASOR_NUMBER: usize = 10;
+const PHASOR_NUMBER: usize = 25;
+const FUNDAMENTAL_FREQ: f64 = 2.*PI / 5.;
+const MAX_RAD: f64 = 100.0;
 
 #[wasm_bindgen]
 #[derive(Clone, Copy, Debug)]
@@ -15,21 +17,20 @@ struct Phasor {
 
 #[wasm_bindgen]
 impl Phasor {
-    pub fn zero() -> Self {
-        Self { real: 0., imag: 0. }
-    }
-    
-    pub fn from_angle(a: f64) -> Self {
-        let (c, s) = a.sin_cos();
-        Self { real: s, imag: c }
+    pub fn from_polar(a: f64, m: f64) -> Self {
+        let (s, c) = a.sin_cos();
+        Self { real: c*m, imag: s*m }
     }
 
-    pub fn scale(&self, n: f64) -> Self {
-        Self { real: self.real*n, imag: self.imag*n }
-    }
-
-    pub fn abs(&self) -> f64 {
+    pub fn magnitude(&self) -> f64 {
         (self.real*self.real + self.imag*self.imag).sqrt()
+    }
+
+    pub fn rotate(&self, n: f64, dt: f64) -> Self {
+        let (s, c) = (-FUNDAMENTAL_FREQ*n*dt).sin_cos();
+        let new_real = self.real*c - self.imag*s;
+        let new_imag = self.real*s + self.imag*c;
+        Self { real: new_real, imag: new_imag }
     }
 }
 
@@ -41,12 +42,22 @@ struct PhasorAnimation {
 #[wasm_bindgen]
 impl PhasorAnimation {
     pub fn randomized() -> Self {
-        Self {
-            phasors: (0..PHASOR_NUMBER)
-                .map(|_i| { random() * 2. * PI })
-                .map(|f| { Phasor::from_angle(f).scale(random()*100.) })
-                .collect()
-        }
+        let phasors = (0..PHASOR_NUMBER)
+            .map(|i| { 
+                Phasor::from_polar(
+                    2.*PI * random(), 
+                    if i == 0 { 0.0 } else { MAX_RAD / (i as f64) }
+                )
+            })
+            .collect();
+        Self { phasors: phasors }
+    }
+
+    pub fn update(&mut self, dt: f64) {
+        self.phasors = self.phasors.iter()
+            .enumerate()
+            .map(|(i, p)| { p.rotate(i as f64, dt) })
+            .collect();
     }
 
     pub fn get_state(&self) -> Box<[Phasor]> {
--- www/index.js
+++ www/index.js
@@ -1,6 +1,8 @@
 import * as wasm from "rust-wasm";
+import { memory } from "rust-wasm/rust_wasm_bg.wasm"
 var svgpath = require('svgpath')
 const getBounds = require('svg-path-bounds')
+const msPerS = 1000
 
 // Components
 let svgPathTextArea = document.getElementById('svgpath')
@@ -12,6 +14,19 @@ let width = canvas.width
 let height = canvas.height
 let originX = width/2
 let originY = height/2
+
+
+function animate(onFrame) {
+    let s = Date.now()
+    function loop() {
+        let dt = (Date.now() - s) / msPerS
+        onFrame({ dt })
+        s = Date.now()
+        requestAnimationFrame(loop)
+    }
+    requestAnimationFrame(loop)
+}
+
 
 
 function getNewPath(e) {
@@ -29,8 +44,6 @@ function getNewPath(e) {
     return transformed
 }
 
-
-
 // Display SVG
 // let path = getNewPath({})
 // ctx.stroke(new Path2D(path))
@@ -43,24 +56,52 @@ function getNewPath(e) {
 // ctx.stroke()
 
 // Draw phasor
-let phasorAnim = wasm.PhasorAnimation.randomized()
-let state = phasorAnim.get_state()
-ctx.beginPath()
-let x = originX
-let y = originY
-ctx.moveTo(x, y)
-for (let p of state) {
-    x += p.real
-    y += p.imag
-    ctx.lineTo(x, y)
-}
-ctx.stroke()
-x = originX
-y = originY
-for (let p of state) {
+let pAni = wasm.PhasorAnimation.randomized()
+
+let points = []
+let maxpoints = 100
+
+animate(({ dt }) => {
+    pAni.update(dt)
+    let state = pAni.get_state()
+
+    ctx.clearRect(0, 0, width, height)
+    
+    ctx.strokeStyle = '#cccccc'
     ctx.beginPath()
-    ctx.arc(x, y, p.abs(), 0, Math.PI*2)
+    let x = originX
+    let y = originY
+    ctx.moveTo(x, y)
+    for (let p of state) {
+        x += p.real
+        y += p.imag
+        ctx.lineTo(x, y)
+    }
     ctx.stroke()
-    x += p.real
-    y += p.imag
-}
+
+    ctx.strokeStyle = '#333333'
+    x = originX
+    y = originY
+    for (let p of state) {
+        ctx.beginPath()
+        ctx.arc(x, y, p.magnitude(), 0, Math.PI*2)
+        ctx.stroke()
+        x += p.real
+        y += p.imag
+    }
+
+    points.push({ x, y })
+    while (points.length >= maxpoints) {
+        points.shift()
+    }
+
+    ctx.strokeStyle = '#ffffff'
+    ctx.lineWidth = 2
+    ctx.beginPath()
+    ctx.moveTo(points[0].x, points[0].y)
+    for (let {x, y} of points.slice(1)) {
+        ctx.lineTo(x, y)
+    }
+    ctx.stroke()
+    ctx.lineWidth = 1
+})
